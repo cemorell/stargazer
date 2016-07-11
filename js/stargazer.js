a8a689335ea9bdbdb544fc8cddc67691c2bf831a
camille morell <cemorell>
2016-07-11 18:54:28 -0500

    done with the help of chris

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,29 @@
+
+function showImage(jason){
+var image = jason.image.src;
+$('#preview').html(`<div><img src="${image}" /> </div>`)
+
+}
+
+
 function search(name) {
   var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
+$.ajax({
+  Method: "Get",
+  url: api,
+  dataType: "jsonp",
+  jsonpCallback: "showImage"
+});
 }
+
+
+
+$(document).ready(function() {
+  $("#submitStar").click(function(event) {
+    event.preventDefault();
+
+    var name = $('#search-name').val();
+    search(name);
+
+  });
+});
